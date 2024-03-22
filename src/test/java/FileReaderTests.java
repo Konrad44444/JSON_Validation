07960839f160db6a333acbd05e2c022574c92888
Konrad44444 <konrad_warzecha@wp.pl>
2024-03-22 15:49:32 +0100

    move resources, test correct json file returns JSONObcject

--- src/test/java/FileReaderTests.java
+++ src/test/java/FileReaderTests.java
@@ -1,5 +1,7 @@
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import com.remitly.FileReader;
@@ -15,6 +17,14 @@ public class FileReaderTests {
     @Test
     public void givenInvalidJsonFile_expectInvalidFileException() {
         assertThrows(InvalidJsonFileException.class,
-                () -> FileReader.readJSONFromFile("src\\main\\resources\\invalidFile.txt"));
+                () -> FileReader.readJSONFromFile("src\\test\\resources\\invalidFile.txt"));
     }
+
+    @Test
+    public void givenCorrectPath_expectJsonObject() {
+        JSONObject jsonObject = FileReader.readJSONFromFile("src\\test\\resources\\example.json");
+
+        assertNotNull(jsonObject);
+    }
+
 }
