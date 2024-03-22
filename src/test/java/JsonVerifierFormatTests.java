f48ad42fae963afbbdc6a55820068fc357bff52b
Konrad44444 <konrad_warzecha@wp.pl>
2024-03-22 16:06:30 +0100

    test json with no policy name field

---
+++ src/test/java/JsonVerifierFormatTests.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import com.remitly.FileReader;
+import com.remitly.JsonVerifier;
+
+public class JsonVerifierFormatTests {
+    private static final String RESOURCES_PATH = "src\\test\\resources\\";
+
+    @Test
+    public void givenJsonWithNoPolicyNameField_expectFalse() {
+        String fileName = "noPolicyName.json";
+        JSONObject jsonObject = FileReader.readJSONFromFile(RESOURCES_PATH + fileName);
+
+        assertFalse(JsonVerifier.verifyFormat(jsonObject));
+    }
+}
