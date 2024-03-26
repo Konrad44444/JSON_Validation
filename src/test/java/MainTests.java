ada22365c6132ee2d3d70b8a7d1db04a7fcd6fbe
Konrad Warzecha <konrad_warzecha@wp.pl>
2024-03-27 00:29:35 +0100

    fixes:

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -16,6 +16,7 @@ public class MainTests {
     public static final String INCORRECT_PATH_EXCEPTION_MESSAGE = "Cannot find file - check path";
     public static final String INVALID_JSON_FILE_EXCEPTION_MESSAGE = "File is not a valid JSON file";
     public static final String INVALID_JSON_FORMAT_STRING = "Format of JSON file is not a AWS::IAM::Role Policy";
+    public static final String PROMPT_STRING = "Type in path to the JSON file: (ex. C:\\Users\\konra\\Desktop\\json_validation\\src\\test\\resources\\example.json)";
 
     @Test
     public void givenCorrectPathToValidJson_expectSuccessString() throws IOException {
@@ -118,6 +119,27 @@ public class MainTests {
         String consoleOutput = byteArrayOutputStream.toString(Charset.defaultCharset());
 
         assertTrue(consoleOutput.contains(INVALID_JSON_FORMAT_STRING));
+
+        out.close();
+    }
+
+    @Test
+    public void givenMainRun_expectPromptString() {
+        String path = "foo";
+
+        ByteArrayInputStream testIn = new ByteArrayInputStream(path.getBytes());
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(byteArrayOutputStream);
+
+        System.setIn(testIn);
+        System.setOut(out);
+
+        Main.main(null);
+
+        String consoleOutput = byteArrayOutputStream.toString(Charset.defaultCharset());
+
+        assertTrue(consoleOutput.contains(PROMPT_STRING));
 
         out.close();
     }
