790f9bff9151aeccf45b70c99a9068d7af9ca620
Konrad Warzecha <konrad_warzecha@wp.pl>
2024-03-26 12:15:04 +0100

    changes to CLI

--- src/main/java/com/remitly/Main.java
+++ src/main/java/com/remitly/Main.java
@@ -9,16 +9,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // String validJsonExample = "src\\main\\resources\\example.json";
-        // String invalidJsonExample = "src\\main\\resources\\invalidFile.txt";
-        // String notRoleJson = "src\\main\\resources\\notRoleJson.json";
-        // String multipleStatements = "src\\main\\resources\\multipleStatements.json";
-
         try {
 
             String path = getPathFromUser();
             proccessFile(path);
-        
+
         } catch (InvalidJsonFileException | IncorrectPathException e) {
             System.out.println(e.getMessage());
         }
@@ -28,19 +23,26 @@ public class Main {
     private static String getPathFromUser() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Type in path to the JSON file: (ex. src\\main\\resources\\example.json)");
+        System.out.println(
+                "Type in path to the JSON file: (ex. C:\\Users\\konra\\Desktop\\json_validation\\src\\test\\resources\\example.json)");
         String path = scanner.nextLine();
 
         scanner.close();
 
         return path;
     }
-    
+
     private static void proccessFile(String path) {
         JSONObject json = FileReader.readJSONFromFile(path);
 
         if (JsonVerifier.verifyFormat(json)) {
-            System.out.println(JsonVerifier.verifyResourceField(json));
+
+            if (JsonVerifier.verifyResourceField(json)) {
+                System.out.println("True - there is no single asterisk in Resource field");
+            } else {
+                System.out.println("False - there is single asterisk in Resource field");
+            }
+
         } else {
             System.out.println("Format of JSON file is not a AWS::IAM::Role Policy");
         }
