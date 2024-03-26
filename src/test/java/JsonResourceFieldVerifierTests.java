78b14fde529861a13a531c60f5fa7db59ab4f71f
Konrad Warzecha <konrad_warzecha@wp.pl>
2024-03-26 11:20:50 +0100

    test single asterisk in resource field

--- src/test/java/JsonResourceFieldVerifierTests.java
+++ src/test/java/JsonResourceFieldVerifierTests.java
@@ -1,3 +1,20 @@
-public class JsonResourceFieldVerifierTests {
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import com.remitly.FileReader;
+import com.remitly.JsonVerifier;
+
+public class JsonResourceFieldVerifierTests {
+    private static final String RESOURCES_PATH = "src\\test\\resources\\";
+
+    @Test
+    public void givenJsonWithSingleAsteriskInResourceField_expoectFalse() {
+        String fileName = "jsonWithSingleAsteriskInResourceField.json";
+
+        JSONObject jsonObject = FileReader.readJSONFromFile(RESOURCES_PATH + fileName);
+
+        assertFalse(JsonVerifier.verifyResourceField(jsonObject));
+    }
 }
