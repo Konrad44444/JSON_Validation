48b5e0e6852c1a28328297822c44a4e0aa9f16c9
Konrad Warzecha <konrad_warzecha@wp.pl>
2024-03-26 13:01:27 +0100

    test main - invalid json format message

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -15,6 +15,7 @@ public class MainTests {
     public static final String FAIL_STRING = "False - there is single asterisk in Resource field";
     public static final String INCORRECT_PATH_EXCEPTION_MESSAGE = "Cannot find file - check path";
     public static final String INVALID_JSON_FILE_EXCEPTION_MESSAGE = "File is not a valid JSON file";
+    public static final String INVALID_JSON_FORMAT_STRING = "Format of JSON file is not a AWS::IAM::Role Policy";
 
     @Test
     public void givenCorrectPathToValidJson_expectSuccessString() throws IOException {
@@ -96,6 +97,27 @@ public class MainTests {
         String consoleOutput = byteArrayOutputStream.toString(Charset.defaultCharset());
 
         assertTrue(consoleOutput.contains(INVALID_JSON_FILE_EXCEPTION_MESSAGE));
+
+        out.close();
+    }
+
+    @Test
+    public void givenInvalidJsonFormat_expectInvalidJsonFormatString() {
+        String path = "C:\\Users\\konra\\Desktop\\json_validation\\src\\test\\resources\\emptyPolicyName.json";
+
+        ByteArrayInputStream testIn = new ByteArrayInputStream(path.getBytes());
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(byteArrayOutputStream);
+
+        System.setIn(testIn);
+        System.setOut(out);
+
+        Main.main(null);
+
+        String consoleOutput = byteArrayOutputStream.toString(Charset.defaultCharset());
+
+        assertTrue(consoleOutput.contains(INVALID_JSON_FORMAT_STRING));
 
         out.close();
     }
